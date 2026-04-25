6b40a941d87997dbc89ce4aba9e005dc70951052
Liu Xiaoyi <circuitcoder0@gmail.com>
2026-04-26 02:11:27 +0800

    WIP: bump to Chisel 7

--- ActiveN/src/main/scala/koneko/Main.scala
+++ ActiveN/src/main/scala/koneko/Main.scala
@@ -49,15 +49,78 @@ object Main extends App {
     try pw.write(content) finally pw.close()
   }
 
-  val emitSystem = sys.env.get("AN_SYSTEM").nonEmpty
-  if (emitSystem) {
-    val numPU = sys.env.get("AN_NUM_PU").flatMap(_.toIntOption).getOrElse(16)
-    val numMC = sys.env.get("AN_NUM_MC").flatMap(_.toIntOption).getOrElse(1)
-    implicit val sysParam = SystemParameters(numMC, numPU, param)
-    ChiselStage.emitSystemVerilogFile(new System, args)
-    writeJson("System.config.json", systemConfigJson(sysParam))
+  // Parse arguments
+  var system: Boolean = false
+  var core: Boolean = false
+  var pu: Option[Int] = None
+  var mc: Option[Int] = None
+  var output: String = "./generated"
+  var spliter = args.indexOf("--")
+  val (mainArgs, otherArgs) = if (spliter >= 0) {
+    this.args.splitAt(spliter)
   } else {
-    ChiselStage.emitSystemVerilogFile(new Core()(param), args)
-    writeJson("Core.config.json", coreConfigJson(param))
+    (args, Array.empty[String])
+  }
+
+  for (arg <- mainArgs) {
+    arg match {
+      case "--help" | "-h" => {
+        println("Usage: [options] [-- <chisel options>]")
+        println("Options:")
+        println("  --core           Emit Core with default config")
+        println("  --system         Emit System with default config")
+        println("  --pu=<N>         Emit System with N PUs")
+        println("  --mc=<N>         Emit System with N MCs")
+        println("  --output=<path>  Set output path for generated files. Default: ./generated")
+        sys.exit(0)
+      }
+      case "--core" => core = true
+      case "--system" => system = true
+      case a if a.startsWith("--pu=") => {
+        pu = a.stripPrefix("--pu=").toIntOption
+        if (pu.isEmpty) {
+          println(s"Invalid PU count: ${a.stripPrefix("--pu=")}")
+          sys.exit(1)
+        }
+      }
+      case a if a.startsWith("--mc=") => {
+        mc = a.stripPrefix("--mc=").toIntOption
+        if (mc.isEmpty) {
+          println(s"Invalid MC count: ${a.stripPrefix("--mc=")}")
+          sys.exit(1)
+        }
+      }
+      case a if a.startsWith("--output=") => {
+        output = a.stripPrefix("--output=")
+        if (output.isEmpty) {
+          println(s"Warning: ignoring empty output path, using './generated'")
+          output = "./generated"
+        }
+      }
+      case _ => {
+        println(s"Unknown argument: $arg")
+        sys.exit(1)
+      }
+    }
+  }
+
+  if (!system && !core) {
+    println("No target specified. Use --system or --core.")
+    sys.exit(1)
+  }
+
+  var chiselArgs = Array("--target-dir", output) ++ otherArgs
+
+  if (system) {
+    if (pu.isEmpty || mc.isEmpty) {
+      println("For system generation, both --pu and --mc must be specified.")
+      sys.exit(1)
+    }
+    implicit val sysParam = SystemParameters(mc.get, pu.get, param)
+    ChiselStage.emitSystemVerilogFile(new System, chiselArgs)
+    writeJson(s"$output/System.config.json", systemConfigJson(sysParam))
+  } else {
+    ChiselStage.emitSystemVerilogFile(new Core()(param), chiselArgs)
+    writeJson(s"$output/Core.config.json", coreConfigJson(param))
   }
 }
