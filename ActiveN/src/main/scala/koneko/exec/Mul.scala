6ac0d869757dd4a76d3e928c919efe97806348e3
Liu Xiaoyi <circuitcoder0@gmail.com>
2026-05-05 07:22:00 +0800

    Fixing multiplier

--- ActiveN/src/main/scala/koneko/exec/Mul.scala
+++ ActiveN/src/main/scala/koneko/exec/Mul.scala
@@ -21,39 +21,15 @@ class Mul extends Module {
   }))
   val output = IO(Output(UInt(32.W)))
 
-  // val cnt = RegInit(0.U(log2Up(MUL_DELAY).W))
-  // val done = Wire(Bool())
-  // cnt := Mux(input.valid && !done, cnt + 1.U, 0.U)
-  // done := cnt === (MUL_DELAY - 1).U
+  val rs1ext = (input.rs1signed && input.rs1(31)) ## input.rs1
+  val rs2ext = (input.rs2signed && input.rs2(31)) ## input.rs2
 
-  // FIXME: Pipeline
-  // input.ready := RegNext(input.valid) // Count cycles
+  val result = (rs1ext * rs2ext)(63, 0)
 
-  val rs1t = input.rs1(31, 16)
-  val rs1b = input.rs1(15, 0)
-  val rs2t = input.rs2(31, 16)
-  val rs2b = input.rs2(15, 0)
-
-  val bb = RegNext(rs1b * rs2b)
-  val tb = RegNext(rs1t * rs2b)
-  val bt = RegNext(rs1b * rs2t)
-  val tt = RegNext(rs1t * rs2t)
-
-  val rs1neg = RegNext(Mux(input.rs1signed && input.rs1(31), input.rs1, 0.U))
-  val rs2neg = RegNext(Mux(input.rs2signed && input.rs2(31), input.rs2, 0.U))
-
-  val s1low = RegNext(input.low)
-  val s1high = RegNext(input.high)
-  val s1mid = RegNext(input.mid)
-
-  val result = (
-    (tt << 32).asUInt + (tb << 16).asUInt + (bt << 16).asUInt + bb
-    - rs1neg - rs2neg
-  )
-
-  output := Mux1H(Seq(
-    s1low -> result(31, 0),
-    s1high -> result(63, 31),
-    s1mid -> result(47, 16),
-  ))
+  // Retimer go brrrrr
+  output := RegNext(Mux1H(Seq(
+    input.low -> result(31, 0),
+    input.high -> result(63, 32),
+    input.mid -> result(47, 16),
+  )))
 }
